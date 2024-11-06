620d93e58402269b10c36d16ee4ab1c9f9ee8b80
Daniel Chudý <xchudy06@vutbr.cz>
2024-11-06 14:27:29 +0100

    Added UTF-8 encoding to the conllu file

--- src/preprocess/tokenizer.py
+++ src/preprocess/tokenizer.py
@@ -10,21 +10,23 @@ DATA_PATH = "data"
 OUTPUT_FILE = os.path.join(DATA_PATH, "conllu_data.conllu")
 BATCH_SIZE = 1000
 
+
 def remove_stopwords(df):
     stop_words = set(stopwords.words('english'))
     nltk.download('stopwords')
-    #TODO: remove stopwords
+    # TODO: remove stopwords
     pass
+
 
 def tokenize(rows=1000, stopword_removal=False):
     df = pd.read_csv(os.path.join(DATA_PATH, "clean_data.csv"))
     df_first_N = df.iloc[:rows]
     tokenize_pretokenized = False
-    
+
     if stopword_removal:
         tokenize_pretokenized = True
         remove_stopwords(df_first_N)
-        
+
     stanza.download('en')
     nlp = stanza.Pipeline(
         'en', processors='tokenize,lemma,pos', verbose=True, use_gpu=True, tokenize_pretokenized=tokenize_pretokenized)
@@ -40,19 +42,19 @@ def tokenize(rows=1000, stopword_removal=False):
         # Create the CoNLL-U formatted header for the row
         conllu_str = f"# movie_id = {row.movie_id}\n"
         conllu_str += f"# genre = {row.encoded_genre}\n"
-        # Formatting borrowed from stanza.utils.conll 
+        # Formatting borrowed from stanza.utils.conll
         conllu_str += "{:C}\n\n".format(doc)
 
         # Add the formatted string to the batch
         conllu_batch.append(conllu_str)
 
         if i % BATCH_SIZE == 0:
-            with open(OUTPUT_FILE, "a") as f:
+            with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
                 f.writelines(conllu_batch)
 
             conllu_batch = []
 
     # Remaining rows if the final batch was less than 1k
     if conllu_batch:
-        with open(OUTPUT_FILE, "a") as f:
+        with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
             f.writelines(conllu_batch)
