811d421af1b3b7f372371e94cba0d3954aa3b114
filip2faber <filip2faber@gmail.com>
2024-11-25 20:16:15 +0100

    autocommit

--- src/evaluation.py
+++ src/evaluation.py
@@ -19,47 +19,68 @@ log.basicConfig(level=log.INFO,
                 datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def get_metrics(y, y_pred, metrics_names: list[str] = ['jaccard', 'hamming', 'accuracy', 'f1', 'precision', 'recall', 'at_least_one']):
+def signed_overlap(y_true, y_pred):
+    """
+    Compute signed overlap metric for multilabel classification.
+
+    Args:
+        y_true (np.ndarray): Ground truth (binary matrix, shape [n_samples, n_classes]).
+        y_pred (np.ndarray): Predictions (binary matrix, shape [n_samples, n_classes]).
+
+    Returns:
+        float: Signed overlap score.
+    """
+    intersection = np.sum(np.logical_and(y_true, y_pred), axis=1)
+    false_negatives = np.sum(np.logical_and(y_true, ~y_pred), axis=1)
+    false_positives = np.sum(np.logical_and(~y_true, y_pred), axis=1)
+    union = np.sum(np.logical_or(y_true, y_pred), axis=1)
+
+    signed_overlap_score = np.mean((intersection - false_negatives - false_positives) / (union + 1e-9))
+    return signed_overlap_score
+
+
+def at_least_k(y_true, y_pred, k: int = 1):
+    """
+    Compute the fraction of samples for which at least one label is predicted correctly.
+
+    Args:
+        y_true (np.ndarray): Ground truth (binary matrix, shape [n_samples, n_classes]).
+        y_pred (np.ndarray): Predictions (binary matrix, shape [n_samples, n_classes]).
+
+    Returns:
+        float: Fraction of samples for which at least one label is predicted correctly
+    """
+    return np.mean(np.any(y_true & y_pred, axis=1))
+
+
+def get_metrics(y_true, y_pred, metrics_names: list[str] = ['jaccard', 'hamming', 'accuracy', 'f1', 'precision', 'recall', 'at_least_one', 'signed_overlap']):
     '''
-    Get metrics for multilabel classification
-    Parameters:
-    y: ndarray of shape (n_samples, n_classes)
-        True labels
-    y_pred: ndarray of shape (n_samples, n_classes)
-        Predicted labels
-    metrics: list of str
-        List of metrics to compute. Default: ['jaccard', 'hamming', 'accuracy', 'f1', 'precision', 'recall']
+    Get metrics for multilabel classification.
+
+    Args:
+        y_true (np.ndarray): Ground truth (binary matrix, shape [n_samples, n_classes]).
+        y_pred (np.ndarray): Predictions (binary matrix, shape [n_samples, n_classes]).
+        metrics_names (list[str]):  List of metrics to compute. Default: ['jaccard', 'hamming', 'accuracy', 'f1', 'precision', 'recall'].
     '''
 
     metrics = {}
     if 'jaccard' in metrics_names:
-        metrics['jaccard'] = jaccard_score(y, y_pred, average='samples')
+        metrics['jaccard'] = jaccard_score(y_true, y_pred, average='samples')
     if 'hamming' in metrics_names:
-        metrics['hamming'] = hamming_loss(y, y_pred)
+        metrics['hamming'] = hamming_loss(y_true, y_pred)
     if 'accuracy' in metrics_names:
-        metrics['accuracy'] = accuracy_score(y, y_pred)
+        metrics['accuracy'] = accuracy_score(y_true, y_pred)
     if 'f1' in metrics_names:
-        metrics['f1'] = f1_score(y, y_pred, average='samples')
+        metrics['f1'] = f1_score(y_true, y_pred, average='samples')
     if 'precision' in metrics_names:
-        metrics['precision'] = precision_score(y, y_pred, average='samples')
+        metrics['precision'] = precision_score(y_true, y_pred, average='samples')
     if 'recall' in metrics_names:
-        metrics['recall'] = recall_score(y, y_pred, average='samples')
+        metrics['recall'] = recall_score(y_true, y_pred, average='samples')
     if 'at_least_one' in metrics_names:
-        metrics['at_least_one'] = at_lest_k(y, y_pred, 1)
-
+        metrics['at_least_one'] = at_least_k(y_true, y_pred, 1)
+    if 'signed_overlap' in metrics_names:
+        metrics['signed_overlap'] = signed_overlap(y_true, y_pred)
     return metrics
-
-
-def at_lest_k(y, y_pred, k: int = 1):
-    '''
-    Compute the fraction of samples for which at least one label is predicted correctly
-    Parameters:
-    y: ndarray of shape (n_samples, n_classes)
-        True labels
-    y_pred: ndarray of shape (n_samples, n_classes)
-        Predicted labels
-    '''
-    return np.mean(np.any(y & y_pred, axis=1))
 
 
 def plot_metric_per_genre(y, y_pred, metrics: list[str]):
@@ -124,20 +145,25 @@ def plot_feature_importances(feat_names: list[str], importances: Union[list[floa
     plt.close()
 
 
-def analyse_features(model: MultiLabelClassifier, text_model: Union[BagOfWords, WordEmbeddingModel], path: str, top_k: int = 10):
+def get_feature_importances(model: MultiLabelClassifier, text_model: Union[BagOfWords, WordEmbeddingModel]):
+    '''
+    Get feature importances for given model and text model.
 
+    Args:
+        model (MultiLabelClassifier) Model for which feature importances are to be computed.
+
+        text_model (Union[BagOfWords, WordEmbeddingModel]): Text model used to vectorize text data.
+
+    Returns:
+        feat_impts (list of np.ndarray): List of feature importances for each classifier in MultiOutputClassifier
+    '''
     feat_impts = []  # contains feature importances for each classifier contained in MultiOutputClassifier
     estimators = model.multi_output_clf_.estimators_
-    feature_names = text_model.get_feature_names_out()
-
-    with open(os.path.join(DATA_PATH, 'genres.json'), 'r') as f:
-        classes = json.load(f)
 
     if hasattr(estimators[0], 'coef_'):
         for clf in estimators:
             # ndarray of shape (1, n_features)
-            feat_impts.append(clf.coef_)
-        log.info(f"Feature importance Shape: {feat_impts[0].shape}")
+            feat_impts.append(clf.coef_[0])
 
     elif hasattr(estimators[0], 'feature_importances_'):
         for clf in estimators:
@@ -146,9 +172,18 @@ def analyse_features(model: MultiLabelClassifier, text_model: Union[BagOfWords, 
     else:
         raise Warning(
             "Model does not have attribute for feature importance. Returning None")
+    return feat_impts
+
+
+def analyse_features(model: MultiLabelClassifier, text_model: Union[BagOfWords, WordEmbeddingModel], path: str, top_k: int = 10):
+
+    feature_names = text_model.get_feature_names_out()
+    feat_impts = get_feature_importances(model, text_model)
+    with open(os.path.join(DATA_PATH, "genres.json"), 'r') as f:
+        classes = json.load(f)
 
     for i, (genre, feat_impt) in enumerate(zip(classes, feat_impts)):
-        feat_impt = feat_impt[0]
+        feat_impt = feat_impt
 
         indices = np.argsort(feat_impt)[::-1][:top_k]
         feat_names = feature_names[indices]
@@ -158,16 +193,18 @@ def analyse_features(model: MultiLabelClassifier, text_model: Union[BagOfWords, 
         plot_wordcloud(feat_names, importances, genre, path)
 
 
-# Wordclouds for each class
-
-
 # Color words in descriptions according to importance
 
+def plot_qualitative_results(model, text_model, X, y, y_pred, n_samples: int = 10):
+    # extract predictions with very bad performance
+    metrics = get_metrics(y, y_pred, 'signed_overlap')
+    bad_indices = np.argsort(metrics)[:n_samples]
+    # get descriptions
+    descriptions = X[bad_indices]
 
-def plot_qualitative_results(model, text_model, X, y, y_pred, n_samples: int = 5):
-    # Get descriptions for which predictions are very bad/ very good
-
-    pass
+    # get feature importances
+    feature_names = text_model.get_feature_names_out()
+    feature_importances = model.multi_output_clf_.estimators_[0].coef_[0]
 
 
 def evaluate(model: MultiLabelClassifier, text_model: Union[BagOfWords, WordEmbeddingModel], y, y_pred):
